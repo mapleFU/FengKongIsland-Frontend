2c1083c271280d4f49d9518d8152e91aa314f983
mapleFU <1506118561@qq.com>
2019-05-09 06:52:23 +0800

    [Fuck] fuck favicon in Vue-cli

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,14 @@ module.exports = {
       .use("raw-loader")
       .loader("raw-loader")
       .end();
+  },
+  pwa: {
+    iconPaths: {
+      favicon32: "https://nmsl.maplewish.cn/favicon.ico",
+      favicon16: "https://nmsl.maplewish.cn/favicon.ico",
+      appleTouchIcon: "https://nmsl.maplewish.cn/favicon.ico",
+      maskIcon: "https://nmsl.maplewish.cn/favicon.ico",
+      msTileImage: "https://nmsl.maplewish.cn/favicon.ico"
+    }
   }
 };
